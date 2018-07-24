5d93d2cc0fcbc1af77278308b922d2924adbcc27
Chris Walls <cwalls2908@gmail.com>
2018-07-24 13:37:34 -0400

    Global csv writing for local store

--- lib/pservices.rb
+++ lib/pservices.rb
@@ -4,4 +4,13 @@ module Pservices
      Pservices::ExampleService 
     ]
   end
+
+
+  def self.write_to_csv(filename: '' , headers: [], row: '', mode: 'a+')
+    CSV.open(filename, mode, write_headers: true, force_quotes: true) do |csv| 
+      csv << headers if csv.count.eql? 0
+      csv << row
+    end
+  end
 end
+
