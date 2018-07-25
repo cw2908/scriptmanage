91c203035f03bba59c0e4ed27011a9c6b8031326
Chris Walls <cwalls2908@gmail.com>
2018-07-25 13:18:34 -0400

    Use templates

--- lib/generators/service_generator.rb
+++ lib/generators/service_generator.rb
@@ -4,8 +4,8 @@ class Rails::ServiceGenerator < Rails::Generators::NamedBase
 
   
   def create_services_file
-    copy_file 'services.rb', "lib/pservices/#{file_name}.rb"
-    copy_file 'services_test.rb', "test/pservices/#{file_name}_test.rb"
+    template 'services.template', "lib/pservices/#{file_name}.rb"
+    template 'services_test.template', "test/pservices/#{file_name}_test.rb"
   end
 
   private 
