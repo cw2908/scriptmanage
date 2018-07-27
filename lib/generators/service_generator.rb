ee4c1dabae6acae15ee6f2d6ced8262335712828
Chris Walls <cwalls2908@gmail.com>
2018-07-27 14:28:30 -0400

    Generator will append to default list and tests

--- lib/generators/service_generator.rb
+++ lib/generators/service_generator.rb
@@ -1,13 +1,28 @@
 class Rails::ServiceGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates',__FILE__)
   argument :name, type: :string
-  class_option :name required: true
+  # class_option :name required: true
 
   
   def create_services_file
     template 'services.template', "lib/pservices/#{file_name}.rb"
     template 'services_test.template', "test/pservices/#{file_name}_test.rb"
   end
+
+  def append_services
+    log 'append_services', name
+    sentinel = 'def list_services
+    ['
+  
+    in_root do
+      gsub_file 'lib/pservices.rb', /(#{Regexp.escape(sentinel)})/mi do |match|
+        "#{match}\n\tPservices::#{name},\n"
+      end
+    end
+  end
+  
+
+
 
   private 
   def file_name
