07389326f6c4bf2eddc995b50110ef8b328acbf3
Chris Walls <cwalls2908@gmail.com>
2018-07-31 14:13:17 -0400

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
-User.create!(email: 'chris.walls@samanage.com', password: ENV['SCRIPTMANAGE_PASSWORD'], approved: true)
+if Rails.env == 'development'
+  User.create!(email: 'chris.walls@samanage.com', password: ENV['SCRIPTMANAGE_PASSWORD'], approved: true)
+end
