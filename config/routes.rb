4f022d8889a6822bc5dbec7794ef763c65ae8126
Chris Walls <cwalls2908@gmail.com>
2018-08-01 11:40:47 -0400

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'application#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  ## For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # authenticate :user do
-  mount ResqueWeb::Engine => "admin/resque_web"
+    mount ResqueWeb::Engine => "admin/resque_web"
   # end
 end
