7dbd9e8dfe3958988c49a43f2d611334d82c648d
Chris Walls <cwalls2908@gmail.com>
2018-08-08 16:26:07 -0400

    Lock down resque

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get '/services',          to: 'services#index'
   post '/services',         to: 'services#create', :defaults => { :format => :json }
   ## For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # authenticate :user do
+  authenticate :user, lambda {|u|  !Rails.env.production? || u.role == 'admin'} do
     mount ResqueWeb::Engine => "admin/resque_web"
-  # end
+  end
 end
