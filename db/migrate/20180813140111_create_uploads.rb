1115796e4ae0afab1a71c6fb829eb085e83b4e2e
Chris W <cwalls2908@gmail.com>
2018-08-16 10:25:33 -0400

    Updates (#19)
    * Cleaning
    
    * Removing assets

--- db/migrate/20180813140111_create_uploads.rb
+++ db/migrate/20180813140111_create_uploads.rb
@@ -1,8 +1,8 @@
 class CreateUploads < ActiveRecord::Migration[5.1]
   def change
     create_table :uploads do |t|
-      t.string      :attachment_type    null: false
-      t.attachment  :file               null: false
+      t.string      :attachment_type,    null: false
+      t.attachment  :file,               null: false
       t.timestamps
     end
   end
--- test/pservices/user_sync_test.rb
+++
@@ -1,7 +0,0 @@
-require 'minitest/autorun'
-
-describe Pservices::UserSync do
-  it 'is a loaded module' do
-    assert_equal(Pservices::UserSync.class, Module)
-  end
-end
