3dc206cce2c35647554ad8d7baa1ad16f5e50178
Chris Walls <cwalls2908@gmail.com>
2018-08-16 12:40:55 -0400

    regex fix

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,5 +1,5 @@
 class Upload < ApplicationRecord
   belongs_to :user
   has_attached_file :file
-  validates_attachment_content_type :file, content_type: /\Acsv\/
+  validates_attachment_content_type :file, content_type: /\Acsv/
 end
