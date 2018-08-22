ba8e301489e0e92c9bab22f299dcfa79deca227c
Chris Walls <cwalls2908@gmail.com>
2018-08-22 12:01:53 -0400

    Merging

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,13 @@ Rails.application.routes.draw do
   # authenticate :user do
   mount ResqueWeb::Engine => "admin/resque_web"
   # end
+
+
+  defaults format: :json do
+    scope module: :api, defaults: {format: :json}, path: 'api' do 
+      scope module: :v1 do
+        resources :webhooks
+      end
+    end
+  end
 end
